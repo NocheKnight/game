618de846bfd355724700202d614b0ff7828fd576
akortysh <akortysh@yandex.ru>
2025-07-05 05:03:16 +0700

    Add load/save functionality + menu options

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,20 +9,20 @@ public class Player : MonoBehaviour
     [SerializeField] private int _health = 100;
     [SerializeField] private int _satiety = 100;
     [SerializeField] private int _maxInventoryWeight = 10;
-    
+
     [Header("Навыки воровства")]
     [SerializeField] private static int _stealthLevel = 1;
     [SerializeField] private static int _pickpocketLevel = 1;
     [SerializeField] private static int _distractionLevel = 1;
-    
+
     [Header("Экономика")]
     [SerializeField] private static int _money = 1000;
     [SerializeField] private int _fine = 0;
-    
+
     private int _crimeRate;
     private PlayerInventory _inventory;
     private PlayerMover _mover;
-    
+
     public int Money => _money;
     public int Fine => _fine;
     public int CrimeRate => _crimeRate;
@@ -30,9 +30,9 @@ public class Player : MonoBehaviour
     public int PickpocketLevel => _pickpocketLevel;
     public int DistractionLevel => _distractionLevel;
     public int MaxInventoryWeight => _maxInventoryWeight;
-    
+
     public bool IsStealing { get; private set; }
-    
+
     public event UnityAction<int> MoneyChanged;
     public event UnityAction<int> FineChanged;
     public event UnityAction<int> CrimeRateChanged;
@@ -40,13 +40,13 @@ public class Player : MonoBehaviour
     public event UnityAction<int> PickpocketLevelChanged;
     public event UnityAction<int> DistractionLevelChanged;
     public event UnityAction PlayerDied;
-    
+
     private void Awake()
     {
         _inventory = GetComponent<PlayerInventory>();
         _mover = GetComponent<PlayerMover>();
     }
-    
+
     private void Start()
     {
         UpdateInventoryWeight();
@@ -65,7 +65,7 @@ public class Player : MonoBehaviour
         _money += money;
         MoneyChanged?.Invoke(_money);
     }
-    
+
     public bool SpendMoney(int amount)
     {
         if (_money >= amount)
@@ -76,18 +76,18 @@ public class Player : MonoBehaviour
         }
         return false;
     }
-    
+
     public void AddFine(int fineAmount)
     {
         _fine += fineAmount;
         FineChanged?.Invoke(_fine);
-        
+
         if (_money >= _fine)
         {
             PayFine();
         }
     }
-    
+
     public void PayFine()
     {
         if (_money >= _fine)
@@ -103,14 +103,14 @@ public class Player : MonoBehaviour
     {
         _crimeRate += crimePoint;
         CrimeRateChanged?.Invoke(_crimeRate);
-        
+
 
         if (_crimeRate >= 100)
         {
             CallPolice();
         }
     }
-    
+
     public void ReduceCrimeRate(int reduction)
     {
         _crimeRate = Mathf.Max(0, _crimeRate - reduction);
@@ -121,7 +121,7 @@ public class Player : MonoBehaviour
     {
         AddCrimeRate(crimePoint);
     }
-    
+
     public bool TryUpgradeStealth()
     {
         int cost = GetStealthUpdateCost();
@@ -135,10 +135,11 @@ public class Player : MonoBehaviour
         return false;
     }
 
-    public int GetStealthUpdateCost() {
+    public int GetStealthUpdateCost()
+    {
         return _stealthLevel * 100;
     }
-    
+
     public bool TryUpgradePickpocket()
     {
         int cost = GetPickpocketUpdateCost();
@@ -151,10 +152,11 @@ public class Player : MonoBehaviour
         return false;
     }
 
-    public int GetPickpocketUpdateCost() {
+    public int GetPickpocketUpdateCost()
+    {
         return _pickpocketLevel * 150;
     }
-    
+
     public bool TryUpgradeDistraction()
     {
         int cost = GetDistractionUpdateCost();
@@ -167,26 +169,27 @@ public class Player : MonoBehaviour
         return false;
     }
 
-    public int GetDistractionUpdateCost() {
-        return _distractionLevel * 120;;
+    public int GetDistractionUpdateCost()
+    {
+        return _distractionLevel * 120; ;
     }
-    
+
     // Методы для стелс-механик
     public float GetStealthBonus()
     {
         return _stealthLevel * 0.1f; // +10% к стелсу за уровень
     }
-    
+
     public float GetPickpocketChance()
     {
         return Mathf.Min(0.9f, _pickpocketLevel * 0.15f); // Максимум 90% шанс
     }
-    
+
     public float GetDistractionEffectiveness()
     {
         return _distractionLevel * 0.2f; // +20% эффективности за уровень
     }
-    
+
     private void UpdateInventoryWeight()
     {
         if (_inventory != null)
@@ -195,7 +198,7 @@ public class Player : MonoBehaviour
             _inventory.UpdateMaxWeight(_maxInventoryWeight);
         }
     }
-    
+
     private void CallPolice()
     {
         var policeCall = FindObjectOfType<PoliceCall>();
@@ -203,7 +206,7 @@ public class Player : MonoBehaviour
         {
             policeCall.Call(_crimeRate);
         }
-        
+
         _crimeRate = 0;
         CrimeRateChanged?.Invoke(_crimeRate);
     }
@@ -223,5 +226,18 @@ public class Player : MonoBehaviour
     private void ResetStealing()
     {
         IsStealing = false;
+    }
+
+    public PlayerSave ToPlayerSave()
+    {
+        return new PlayerSave(_stealthLevel, _pickpocketLevel, _distractionLevel, _money);
+    }
+
+    public void FromPlayerSave(PlayerSave save)
+    {
+        _stealthLevel = save.stealthLevel;
+        _pickpocketLevel = save.pickpocketLevel;
+        _distractionLevel = save.distractionLevel;
+        _money = save.money;
     }
 }
---
+++ Assets/Scripts/UI/SaveLoad.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+using System;
+
+[RequireComponent(typeof(Player))]
+public class SaveLoad : MonoBehaviour
+{
+    private Player _player;
+
+    void Awake() {
+        _player = GetComponent<Player>();
+    }
+
+    void Start()
+    {
+        UpdateUI();
+    }
+
+    void UpdateUI() {
+        for (int i = 0; i < 3; ++i)
+        {
+            UpdateSave(i);
+        }
+    }
+
+    private void UpdateSave(int slotIdx)
+    {
+        PlayerSave save = LoadSaveSlot(slotIdx);
+        GameObject.Find(string.Format("Save{0} Last Update", slotIdx + 1)).GetComponent<TMPro.TextMeshProUGUI>().text =
+        "Last update: " + ((DateTime)save.lastUpdateTime).ToLocalTime().ToString("dd.MM.yyyy HH:mm");
+    }
+
+    public void Save(int slotIdx)
+    {
+        string jsonSave = JsonUtility.ToJson(_player.ToPlayerSave());
+        var writer = new StreamWriter("Assets/Resources/Saves/save" + (slotIdx + 1).ToString() + ".json");
+        writer.WriteLine(jsonSave);
+        writer.Close();
+
+        UpdateSave(slotIdx);
+    }
+
+    private PlayerSave LoadSaveSlot(int slotIdx)
+    {
+        var reader = new StreamReader("Assets/Resources/Saves/save" + (slotIdx + 1).ToString() + ".json");
+        var save = JsonUtility.FromJson<PlayerSave>(reader.ReadLine());
+        reader.Close();
+
+        return save;
+    }
+
+    public void Load(int slotIdx)
+    {
+        _player.FromPlayerSave(LoadSaveSlot(slotIdx));
+
+        GoToMenu();
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
